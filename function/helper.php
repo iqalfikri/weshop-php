bf15b9d5b073644d80b7fcb42a0f512fad088680
iqalfikri <haiqal@mexious.com>
2021-11-09 16:21:40 +0700

    Create Detail Barang

--- function/helper.php
+++ function/helper.php
@@ -1,8 +1,30 @@
 <?php
+require_once "koneksi.php";
 
 define ("BASE_URL", "http://localhost/weshop/");
 
 function rupiah($nilai = 0) {
     $string = "Rp, " . number_format($nilai);
     return $string;
+}
+
+function kategori($kategori_id = false) {
+    global $db;
+   $string = "<div id='menu-kategori'>";
+
+       $string .= "<ul>";
+       
+            $query = mysqli_query($db, "SELECT * FROM kategori WHERE status='on'");
+            while ($row = mysqli_fetch_assoc($query)) {
+                if ($kategori_id == $row['kategori_id']) {
+                    $string .= "<li><a href='".BASE_URL."index.php?kategori_id=$row[kategori_id]' class='active'>$row[kategori]</a></li>";
+                } else {
+                    $string .= "<li><a href='".BASE_URL."index.php?kategori_id=$row[kategori_id]'>$row[kategori]</a></li>";
+                }
+            }
+$string .= "</ul>";
+
+$string .= "</div>";
+
+return $string;
 }
--- main.php
+++ main.php
@@ -1,20 +1,7 @@
 <div id="kiri">
-    <div id="menu-kategori">
-
-        <ul>
-            <?php
-            $query = mysqli_query($db, "SELECT * FROM kategori WHERE status='on'");
-            while ($row = mysqli_fetch_assoc($query)) {
-                if ($kategori_id == $row['kategori_id']) {
-                    echo "<li><a href='".BASE_URL."index.php?kategori_id=$row[kategori_id]' class='active'>$row[kategori]</a></li>";
-                } else {
-                    echo "<li><a href='".BASE_URL."index.php?kategori_id=$row[kategori_id]'>$row[kategori]</a></li>";
-                }
-            }
-         ?>
-        </ul>
-
-    </div>
+    <?php
+        echo kategori($kategori_id); 
+    ?>
 </div>
 <div id="kanan">
 
@@ -46,11 +33,11 @@
                                 $no = 0;                            }
                             echo"<li $style>
                                             <p class='price'>".rupiah($row['harga'])."</p>
-                                            <a herf='".BASE_URL."index.php?page=detail&barang_id=$row[barang_id]'>
+                                            <a href='".BASE_URL."index.php?page=detail&barang_id=$row[barang_id]'>
                                                 <img src='".BASE_URL."img/barang/$row[gambar]'/>
                                             </a>
                                             <div class='keterangan-gambar'>
-                                                <p><a herf='".BASE_URL."index.php?page=detail&barang_id=$row[barang_id]'>$row[nama_barang]</a></p>
+                                                <p><a href='".BASE_URL."index.php?page=detail&barang_id=$row[barang_id]'>$row[nama_barang]</a></p>
                                                 <span>Stok : $row[stok]</span>
                                             </div>
                                             <div class='button-add-cart'>
