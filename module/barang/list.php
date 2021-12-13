6838100e4a77de42e60a3d01e8cb2d7d28314308
iqalfikri <haiqal@mexious.com>
2021-12-13 21:14:44 +0700

    adding search form

--- module/barang/list.php
+++ module/barang/list.php
@@ -1,13 +1,36 @@
+<?php
+    $search = isset($_GET["search"]) ? $_GET["search"] : false;
+
+    $where = "";
+    $search_url = "";
+    if ($search) {
+        $search_url = "&search=$search";
+        $where = "WHERE barang.nama_barang LIKE'%$search%'";
+    }
+?>
 <div id="frame-tambah">
-    <a href="<?= BASE_URL ; ?>index.php?page=my_profile&module=barang&action=form" class="tombol-action">+ Tambah
-        Barang</a>
+    <div id="left">
+        <form action="<?= BASE_URL."index.php"?>" method="GET">
+            <input type="hidden" name="page" value="<?= $_GET["page"]; ?>" />
+            <input type="hidden" name="module" value="<?= $_GET["module"]; ?>" />
+            <input type="hidden" name="action" value="<?= $_GET["action"]; ?>" />
+            <input type="text" name="search" value="<?= $search; ?>" />
+            <input type="submit" value="search" />
+        </form>
+    </div>
+    <div id="right">
+        <a href="<?= BASE_URL ; ?>index.php?page=my_profile&module=barang&action=form" class="tombol-action">+ Tambah
+            Barang</a>
+    </div>
 </div>
 <?php
     $pagination = isset($_GET["pagination"]) ? $_GET["pagination"] : 1;
     $data_per_halaman = 5;
     $mulai_dari = ($pagination - 1) * $data_per_halaman; 
 
-    $query = mysqli_query($db, "SELECT barang.*, kategori.kategori FROM barang JOIN kategori ON barang.kategori_id=kategori.kategori_id ORDER BY nama_barang ASC LIMIT $mulai_dari, $data_per_halaman");
+    
+
+    $query = mysqli_query($db, "SELECT barang.*, kategori.kategori FROM barang JOIN kategori ON barang.kategori_id=kategori.kategori_id $where ORDER BY nama_barang ASC LIMIT $mulai_dari, $data_per_halaman");
 
     if (mysqli_num_rows($query) == 0) {
         echo "<h3>Saat ini belum ada barang di dalam table kategori</h3>";
@@ -38,7 +61,7 @@
         }
         echo"</table>";
 
-        $queryHitungBarang = mysqli_query($db, "SELECT * FROM barang");
-        pagination($queryHitungBarang, $data_per_halaman, $pagination, "index.php?page=my_profile&module=barang&action=list");
+        $queryHitungBarang = mysqli_query($db, "SELECT * FROM barang $where");
+        pagination($queryHitungBarang, $data_per_halaman, $pagination, "index.php?page=my_profile&module=barang&action=list$search_url");
     }
 ?>
