e5935066d15ce02c6c594b912724f0a8ab4f3974
iqalfikri <haiqal@mexious.com>
2021-12-16 00:32:24 +0700

    fixed barang image

--- module/barang/form.php
+++ module/barang/form.php
@@ -86,4 +86,13 @@
 
 <script>
 CKEDITOR.replace("editor");
+
+// var roxyFileman = 'js/ckeditor/fileman/index.html';
+// $(function() {
+//     CKEDITOR.replace('editor', {
+//         filebrowserBrowseUrl: roxyFileman,
+//         filebrowserImageBrowseUrl: roxyFileman + '?type=image',
+//         removeDialogTabs: 'link:upload;image:upload'
+//     });
+// });
 </script>
